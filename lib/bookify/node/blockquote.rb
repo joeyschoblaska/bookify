fd01d2e4dd607aa3fd13bc58c2a6bee2ff556a33
Joey Schoblaska <joey.schoblaska@gmail.com>
2017-08-06 15:57:13 -0500

    use prawn-table for blockquotes

--- lib/bookify/node/blockquote.rb
+++ lib/bookify/node/blockquote.rb
@@ -3,12 +3,18 @@ module Bookify::Node
     def render
       font :primary
 
-      bounding_box [bounds.left + 10, cursor], width: bounds.width - 10 do
-        node.css("p").each do |p|
-          text p.inner_html.strip, inline_format: true
-          move_down 10
-        end
+      data = node.css("p").map { |p| [" ", p.inner_html.strip] }
+
+      table(data) do
+        cells.inline_format = true
+        cells.borders = []
+        cells.padding = 5
+
+        column(0).borders = [:left]
+        column(0).border_width = 0.5
       end
+
+      move_down 10
     end
   end
 end
--- readme.md
+++ readme.md
@@ -2,6 +2,4 @@
 A gem for converting Markdown documents to book style, two-column PDFs.
 
 ## TODO
-* vertical stroke on the left of blockquotes
-* blockquotes that cross page boundaries
 * finish converting support for images
