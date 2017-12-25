5ced366cca64339c1fc81210238c7c13180a59cf
Joey Schoblaska <joey.schoblaska@gmail.com>
2017-12-25 05:20:06 -0600

    allow for a paragraph that's just a list of images

--- lib/bookify/node/paragraph.rb
+++ lib/bookify/node/paragraph.rb
@@ -1,8 +1,10 @@
 module Bookify::Node
   class Paragraph < Base
     def render
-      if node.children.count == 1 && node.children.first.name.to_sym == :img
-        SUBCLASSES[:img].render(node.children.first, pdf)
+      if node.children.all? { |c| c.name.to_sym == :img || c.inner_html == "" }
+        node.children.select { |c| c.name.to_sym == :img }.each do |img|
+          SUBCLASSES[:img].render(img, pdf)
+        end
       else
         html = clean_html(node.inner_html)
         font :primary
