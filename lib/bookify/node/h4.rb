0a43f53cbb1854c2668a4c8612141fc24daec209
Joey Schoblaska <joey.schoblaska@gmail.com>
2020-06-16 09:33:32 -0500

    more headroom for headers

--- lib/bookify/node/h4.rb
+++ lib/bookify/node/h4.rb
@@ -3,7 +3,7 @@ require_relative "base"
 module Bookify::Node
   class H4 < Base
     def render
-      move_down 5 unless pdf.y == page_top
+      move_down 10 unless pdf.y == page_top
       break_if_close_to_bottom
 
       font :h4 do
