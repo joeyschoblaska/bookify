e5e99701f6088646602082d37d01f754e573a4d4
Joey Schoblaska <joey.schoblaska@gmail.com>
2020-06-15 20:32:25 -0500

    support images over http

--- lib/bookify/node/image.rb
+++ lib/bookify/node/image.rb
@@ -1,15 +1,23 @@
 require_relative "base"
+require "open-uri"
 
 module Bookify::Node
   class Image < Base
     def render
       options = {
         position: html_classes.include?("center") ? :center : :left,
-        fit: [bounds.width, -1],
+        fit: [bounds.width, -1]
       }
 
-      image node.attr(:src), options
+      image image_from_node(node), options
       move_down 15
+    end
+
+    private
+
+    def image_from_node(node)
+      src = node.attr(:src)
+      /^https?\:\/\//.match?(src) ? URI.open(src) : src
     end
   end
 end
